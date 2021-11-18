7f88d2fbe01088be7fab0dcb511ae3c58c0724bc
zugexiaodui <luyue163@126.com>
2021-11-18 23:07:01 +0800

    finish prepare_data

---
+++ 0-FeatureExtraction/2-prepare_data/extract_frames.py
@@ -0,0 +1,51 @@
+import cv2
+import os
+from os.path import join, exists
+from multiprocessing import Pool
+import argparse
+import warnings
+
+parser = argparse.ArgumentParser("Extract frames.")
+parser.add_argument('--video_dir', type=str, required=True,
+                    help="The dir containing videos. E.g. 'video_dir'/[01_001.avi, 01_002.avi, ...]")
+parser.add_argument('--frame_dir', type=str, required=True,
+                    help="The dir to save frames. All the frames of a video will be saved in a directory. E.g. 'frame_dir'/[01_001/, 01_002/, ...]")
+parser.add_argument('--skip_first', action='store_true',
+                    help="Whether to skip the first frame or not. **For Corridor dataset, please use this option since the first frame is black.**")
+parser.add_argument('--workers', type=int, default=48,
+                    help="The number of processes.")
+
+args = parser.parse_args()
+video_dir: str = args.video_dir
+frame_dir: str = args.frame_dir
+skip_first: bool = args.skip_first
+workers: int = args.workers
+
+
+def extract_frames(video_name: str):
+    video_path = join(video_dir, video_name)
+    video_cap = cv2.VideoCapture(video_path)
+    suc, frame = video_cap.read()
+
+    dst_dir = join(frame_dir, video_name.split('.')[0])
+    if not exists(dst_dir):
+        os.mkdir(dst_dir)
+
+    if skip_first:  # skip the first black frame in Corridor dataset
+        suc, frame = video_cap.read()
+
+    i_frame = 0
+    while suc:
+        cv2.imwrite(join(dst_dir, f"{str(i_frame).zfill(6)}.jpg"), frame, (cv2.IMWRITE_JPEG_QUALITY, 100))
+        i_frame += 1
+        suc, frame = video_cap.read()
+
+
+if __name__ == "__main__":
+    warnings.warn("Please use `--skip_first` if the dataset is Corridor.")
+    video_list = sorted(os.listdir(video_dir))
+    pool = Pool(workers)
+    for video_name in video_list:
+        pool.apply_async(extract_frames, (video_name,))
+    pool.close()
+    pool.join()
--- 0-FeatureExtraction/readme.txt
+++ 0-FeatureExtraction/readme.txt
@@ -1,14 +1,18 @@
 0.  Install SlowFast (https://github.com/facebookresearch/SlowFast).
 
-1.  Convert .pkl format checkpoint to .pth
-    1)  Download the SlowFast Network pre-trained on Kinetics-400 dataset.
-        See https://github.com/facebookresearch/SlowFast/blob/main/MODEL_ZOO.md
-        "SLowFast: Kinetics/c2/SLOWFAST_8x8_R50" URL: https://dl.fbaipublicfiles.com/pyslowfast/model_zoo/kinetics400/SLOWFAST_8x8_R50.pkl
-    2)  Put the downloaded `SLOWFAST_8x8_R50.pkl` file in `pretrainings`.
-        Put `configs/Kinetics/SLOWFAST_8x8_R50.yaml` (in the slowfast installation directory) in `pretrainings`.
-    3)  Change directory to `1-cvt_pkl2pth`. Run `cvt_pkl2pth.sh`.
-        Then `SLOWFAST_8x8_R50.pth` is generated in `pretrainings`.
+1.  Prepare the pre-trained model.
+    1)  Download the SlowFast Network pre-trained on Kinetics-400 dataset. See `1-prepare_model/model_urls.txt`. Put it in `pretrainings/`.
+    2)  Put `slowfast_installation_dir/configs/Kinetics/SLOWFAST_8x8_R50.yaml` in `pretrainings/`.
+    3)  Convert '.pkl' format checkpoint to '.pth' file. Cd `1-prepare_model` and run `cvt_pkl2pth.sh`. 
+        Then `SLOWFAST_8x8_R50.pth` is generated in `pretrainings/`.
 
+2.  Prepare data
+    1)  Download Avenue, ShanghaiTech and Corridor datasets. See `2-prepare_data/dataset_urls.txt`.
+    2)  Cd `2-prepare_data` and run `extract_frames.py` to extract frames for videos. Use `--help` to see the usage.
+        ST: only need to do this for the training videos.
+        Avenue: extract frames for both training & testing videos.
+        Corridor: use `mvfile_corridor.py` to put all the videos in one directory first, and then extract frames for training & testing videos (use `--skip_first` option).
+    3)  Convert the groundtruth labels to '.npz' file.
+        We have done this cumbersome step. The '.npz' files can be seen in `groundtruths/`.
 
-
-
+3. 
